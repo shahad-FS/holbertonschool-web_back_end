2dcba1ca84dbdfc5d2bcd39920e08b0ce01724a1
shahad-FS <alsaneea.s@gmail.com>
2026-02-17 09:15:32 +0000

    fix: documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
+
 """
 add function with arguments that supports typing
 """
-def add (a: float, b: float) -> float:
+def add(a: float, b: float) -> float:
+    """
+    add function with two arguments of float value
+    return : sum of a and b 
+    """
     return a + b
