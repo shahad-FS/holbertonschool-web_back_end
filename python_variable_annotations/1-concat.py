4f1f88a45dc7b4f541cf160e41847a0856ba8314
shahad-FS <alsaneea.s@gmail.com>
2026-02-17 09:22:21 +0000

    Create concat function

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+"""
+function support typeing
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+
+
+    """
+        concat function return the concatenation of
+        two string arguments
+    """
+    return str1 + str2
