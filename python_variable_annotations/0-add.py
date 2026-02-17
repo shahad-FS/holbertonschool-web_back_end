0da58e70ebb0ad98fbda3ece12d107e4c1437fdc
shahad-FS <alsaneea.s@gmail.com>
2026-02-17 09:17:59 +0000

    fix: pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -4,9 +4,9 @@
 """
 add function with arguments that supports typing
 """
+
+
 def add(a: float, b: float) -> float:
-
-
     """
     add function with two arguments of float value
     return : sum of a and b
