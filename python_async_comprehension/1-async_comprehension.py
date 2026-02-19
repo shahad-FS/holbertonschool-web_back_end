b6e9769b99d7f02b0abc5cf67e563cf5d53c0b39
shahad-FS <alsaneea.s@gmail.com>
2026-02-19 23:23:20 +0000

    Create async_comprehension function

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+coroutine called async_comprehension that takes no arguments.
+"""
+
+
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Return List of values yelded by async_generator"""
+    return[value async for value in async_generator]
