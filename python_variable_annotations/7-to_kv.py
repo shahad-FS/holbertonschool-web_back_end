1b6c35bc5e397eeaec8141e35d16c2d6dadbebc7
shahad-FS <alsaneea.s@gmail.com>
2026-02-19 17:15:50 +0000

    Create to_kv function

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+function to_kv that takes a string k and an int OR float
+v as arguments and returns a tuple
+"""
+
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float] -> Tuple[str, float]:
+        """Return tuple"""
+        return (k, v * v)
