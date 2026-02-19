860456fee37b41043914437648d304dbfd8f706a
shahad-FS <alsaneea.s@gmail.com>
2026-02-19 23:07:26 +0000

    Create task_wait_n function

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -5,14 +5,14 @@ except task_wait_random is being called.
 """
 
 
-from typing impor List
+from typing import List
 import asyncio
 import random
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n:int, max_delay: int = 10) -> List[float]:
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
     """Execute task_wait_random and return sorted list of delay"""
     tasks = [task_wait_random(max_delay) for _ in rang(n)]
     delays = []
