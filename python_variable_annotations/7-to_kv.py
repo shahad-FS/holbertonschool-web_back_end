11453de135ee4a0b3f91d45cf3593174ba00aa45
shahad-FS <alsaneea.s@gmail.com>
2026-02-19 17:16:50 +0000

    Create to_kv function

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -8,6 +8,6 @@ v as arguments and returns a tuple
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float] -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
         """Return tuple"""
         return (k, v * v)
