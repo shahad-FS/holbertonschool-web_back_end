8042a93150c85d464dd22eb506a4b4ad642fcfce
shahad-FS <alsaneea.s@gmail.com>
2026-02-19 15:20:55 +0000

    create floor function

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+unction floor which takes a float n as argument and returns the floor of the float.
+"""
+
+
+def floor(n: float) -> float:
+    "return largest value"
+    return int(n) if n >= 0 else int(n) - 1
