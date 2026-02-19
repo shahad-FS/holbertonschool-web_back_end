9e21159d2a25853bdf0d4af211d04fbb52b65c2b
shahad-FS <alsaneea.s@gmail.com>
2026-02-19 17:52:43 +0000

    Annotate safley_get_value function

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -11,7 +11,7 @@ T = TypeVar('T')
 
 
 def safely_get_value(dct: Mapping, key: Any,
-                    default: Union[T, None] = None) -> Union[Any, T]:
+                     default: Union[T, None] = None) -> Union[Any, T]:
     """Return dct[key] if exists"""
     if key in dct:
         return dct[key]
