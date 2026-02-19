d9b64a0a7bf1a1fdc39dc4e6ae3b7bd11d789b18
shahad-FS <alsaneea.s@gmail.com>
2026-02-19 22:44:22 +0000

    Create wait_n function

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -16,7 +16,6 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """Waits for ran delay until max_delay,
     Return: List of actual delay
