ceb5e85e7def1f45f4b7245f7b559f70f2528806
shahad-FS <alsaneea.s@gmail.com>
2026-03-13 21:03:48 +0000

    Create createReportObject function

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,9 +1,8 @@
 export default function createReportObject(employeesList) {
-  return {
-    allEmployees: { ...employeesList, },
-    getNumberOfDepartments(employees) {
-      return;
-Object.keys(employees).length;
-    },
-  };
+	return {
+		allEmployees: { ...employeesList },
+		getNumberOfDepartments(employeesList) {
+			return Object.keys(employeesList).length;
+		},
+	};
 }
