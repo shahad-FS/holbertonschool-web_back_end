cf1c0f560fcf811f86922e89a3319e2ab6898cf0
shahad-FS <alsaneea.s@gmail.com>
2026-04-02 06:59:31 +0000

    Create createInt8TypedArray function

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,13 @@
+function createInt8TypedArray(length, position, value){
+	const buffer = new ArrayBuffer(length);
+	const view = new DataView(buffer, 0);
+
+	if (position > length - 1) {
+		throw Error('Postion outside range');
+	}
+	
+	view.setInt8(postion, value);
+
+	return view;
+}
+export default createInt8TypedArray;
