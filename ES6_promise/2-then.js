e7dd63847c582193c962a9e73d3b61a36ffaeca5
shahad-FS <alsaneea.s@gmail.com>
2026-04-03 21:50:46 +0000

    fix: error

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -4,13 +4,8 @@ function handleResponseFromAPI(promise) {
       status: 200,
       body: 'success',
     }))
-    .catch(() => {
-      return new Error();
-    })
-    .then((result) => {
-      console.warn('Got a response from the API');
-      return result;
-    });
+    .catch(() => new Error())
+    .finally(console.warn.bind(console, 'Got a response from the API'));
 }
 
 export default handleResponseFromAPI;
