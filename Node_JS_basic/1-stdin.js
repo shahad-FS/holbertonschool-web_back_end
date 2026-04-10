fbeab68e2a688848d97a0db9a052fa23e0ea1b99
shahad-fs <alsaneea.s@gmail.com>
2026-04-10 09:26:43 +0000

    Create program named stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,14 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding("utf-8");
+process.stdin.setEncoding('utf8');
 
-process.stdin.on("readable", () => {
+process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name !== null) {
-      console.log(`Your name is: ${name}`);
-    }
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
