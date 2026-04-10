390b6ed6c3bf31cadcefa35b038738f15b8675d9
shahad-fs <alsaneea.s@gmail.com>
2026-04-10 11:49:27 +0000

    Create simple server with express

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
 
+const countStudents = require('./3-read_file_async');
 const args = process.argv.slice(2);
+
 const DATABASE = args[0];
 
 const app = express();
@@ -9,7 +10,7 @@ const port = 1245;
 
 app.get("/", (req, res) => {
   res.send("Hello Holberton School!");
-  });
+});
 
 app.get('/students', async (req, res) => {
   const msg = 'This is the list of our students\n';
