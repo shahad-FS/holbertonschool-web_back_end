d315eb5c6d3ec2b764b7a01355531f3f02342b5e
shahad-fs <alsaneea.s@gmail.com>
2026-04-10 10:56:26 +0000

    Create simple server

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const port = 1245;
 
-const app = http.createServerه((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
